c5bdfa86abbba9a54fd2bcc04518fcc38d27e4fb
Alexander <spier1994@gmail.com>
2021-11-15 22:25:20 +0300

    подправил сервис тегов и контроллер

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,4 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { IMessage } from 'src/address/dto/create-address.dto';
+import { Tags } from 'src/entity/tags.entity';
 import { createTagsDto } from './dto/create-tags.dto';
 import { TagsService } from './tags.service';
 
@@ -7,22 +9,22 @@ export class TagsController {
     constructor(private readonly tagsService: TagsService) { }
 
     @Post()
-    create(@Body() tag: createTagsDto): Promise<number | {}> {
+    create(@Body() tag: createTagsDto): Promise<IMessage> {
         return this.tagsService.createTag(tag);
     }
 
     @Get()
-    getAll() {
+    getAll(): Promise<Tags[]> {
         return this.tagsService.getAllTags();
     }
 
     @Get(':id')
-    getOne(@Param('id') name: string) {
+    getOne(@Param('id') name: string): Promise<Tags> {
         return this.tagsService.getOneTag(name);
     }
 
     @Delete(':id')
-    delete(@Param() id: number) {
+    delete(@Param() id: number): Promise<IMessage> {
         return this.tagsService.deleteTag(id);
     }
 }
--- src/tags/tags.service.ts
+++ src/tags/tags.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { IMessage } from 'src/address/dto/create-address.dto';
 import { Tags } from 'src/entity/tags.entity';
 import { Repository } from 'typeorm';
 import { createTagsDto } from './dto/create-tags.dto';
@@ -8,29 +9,30 @@ import { createTagsDto } from './dto/create-tags.dto';
 export class TagsService {
     constructor(@InjectRepository(Tags) private readonly repo: Repository<Tags>) { }
 
-    async createTag(dto: createTagsDto): Promise<number | {}> {
+    async createTag(dto: createTagsDto): Promise<IMessage> {
         try {
             const res = await this.repo.save({
                 name: dto.name,
                 category: { id: dto.categoryId }
             })
-            return res.id
+            return { message: 'Тег добавлен' }
         } catch (e) {
             console.log(e);
-            return { message: "Ошибка создания тэга" }
+            return { message: 'Ошибка добавления тэга' }
         }
 
     }
 
-    async getAllTags() {
-        return this.repo.find();
+    async getAllTags(): Promise<Tags[]> {
+        return await this.repo.find();
     }
 
-    async getOneTag(name: string) {
-        return this.repo.findOneOrFail(name, { relations: ['category', 'place'] });
+    async getOneTag(name: string): Promise<Tags> {
+        return await this.repo.findOneOrFail(name, { relations: ['category', 'place'] });
     }
 
-    async deleteTag(id: number) {
-        return this.repo.delete(id);
+    async deleteTag(id: number): Promise<IMessage> {
+        const tag = await this.repo.delete(id);
+        return { message: 'Тег удален' }
     }
 }
