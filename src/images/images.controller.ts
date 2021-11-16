85c4969084976a2662ffa90aaedfd416d3e49fc6
Alexander <spier1994@gmail.com>
2021-11-16 20:45:42 +0300

    добавил возврат добавленного элемента

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { ImagesService } from './images.service';
 
 @Controller('images')
-export class ImagesController {}
+export class ImagesController {
+    constructor( private service: ImagesService){ }
+}
