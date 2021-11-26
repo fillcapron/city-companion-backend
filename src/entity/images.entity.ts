e1b70b9d68f6e49d65c7bdd3a4e50d7977314beb
Alexander <spier1994@gmail.com>
2021-11-26 21:35:41 +0300

    правки

--- src/entity/images.entity.ts
+++ src/entity/images.entity.ts
@@ -7,14 +7,8 @@ export class Images extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column({nullable: false, type: 'varchar', length: 55})
-    title: string;
-
     @Column({nullable: false, type: 'varchar', length: 255})
-    full_size: string;
-
-    @Column({nullable: false, type: 'varchar', length: 255})
-    small_size: string;
+    url: string;
 
     @ManyToOne(() => Places, places => places.images, { onDelete: 'CASCADE' })
     place: Places;
